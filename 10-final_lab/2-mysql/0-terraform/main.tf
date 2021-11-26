1afa8a4ba07a71f394e0da374469d2d9e5db9619
guiagb <guilherme.agb94@gmail.com>
2021-11-26 10:42:12 -0300

    alteracoes do juliano de ontem

--- 10-final_lab/2-mysql/0-terraform/main.tf
+++ 10-final_lab/2-mysql/0-terraform/main.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "acessos_g4_mysql" {
     },
     {
       description      = "SSH from Jenkins"
-      from_port        = 0
-      to_port          = 0
+      from_port        = 22
+      to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
